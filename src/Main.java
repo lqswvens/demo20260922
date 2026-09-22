fe837c976a637ea92d8fdf3d12663cf2d184ee55
zccheng <923434699@qq.com>
2026-09-22 16:02:27 +0800

    改main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+/**
+ * Demo 项目入口：先打印欢迎语，再按顺序输出 1~{@value #MAX_COUNT} 的计数。
+ */
 public class Main {
-    public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+    /** 计数上限。 */
+    private static final int MAX_COUNT = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= MAX_COUNT; i++) {
             System.out.println("i = " + i);
         }
     }
